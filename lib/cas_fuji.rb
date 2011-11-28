cdf11e8737482c0b1bc6f4b8089d738d31f477d0
Akash Manohar J <akash@akash.im>
2011-11-28 23:32:03 +0530

    required consumable module in lib/cas_fuji.rb; reversed the require of model files to aid ProxyTicket's requirement of ServiceTicket
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/cas_fuji.rb
+++ lib/cas_fuji.rb
@@ -1,14 +1,17 @@
 require 'sinatra'
 require 'builder'
+require 'uuid'
 require 'cgi'
 require 'ap'
 require 'sinatra/activerecord'
+
 # Load app config
 require './lib/cas_fuji/config'
 
 # Load sinatra app
 require './lib/cas_fuji/app'
 
-# Require all of the models
-Dir["#{Dir.pwd}/lib/models/**/*.rb"].each { |file| require file }
+require 'consumable'
 
+# Require all of the models
+Dir["#{Dir.pwd}/lib/models/**/*.rb"].reverse.each { |file| require file }
