a08ff6d0284d5ca7a7f4402c77b81d890151a804
Sean Grove <s@bushi.do>
2011-12-03 00:43:40 -0800

    Removes doubly-nested cas_fuji directory and simplifies project layout

--- lib/cas_fuji.rb
+++ lib/cas_fuji.rb
@@ -11,13 +11,13 @@ require 'active_support/memoizable'
 require 'active_record'
 require 'addressable/uri'
 
-require 'cas_fuji/exception'
+require_relative 'exception'
 
 # Load app config
-require 'cas_fuji/config'
+require_relative 'config'
 
 # Load the base authenticator
-require 'cas_fuji/authenticators/base'
+require_relative 'authenticators/base'
 
 # Load the authenticators specified in the config file
 CasFuji.config[:authenticators].each do |authenticator|
@@ -26,12 +26,12 @@ CasFuji.config[:authenticators].each do |authenticator|
 end
 
 # Load the models
-require "cas_fuji/models/base_ticket"
-require "cas_fuji/models/login_ticket"
-require "cas_fuji/models/service_ticket"
-require "cas_fuji/models/proxy_ticket"
-require "cas_fuji/models/proxy_granting_ticket"
-require "cas_fuji/models/ticket_granting_ticket"
+require_relative "models/base_ticket"
+require_relative "models/login_ticket"
+require_relative "models/service_ticket"
+require_relative "models/proxy_ticket"
+require_relative "models/proxy_granting_ticket"
+require_relative "models/ticket_granting_ticket"
 
 # Load sinatra app
-require 'cas_fuji/app'
+require_relative 'app'
