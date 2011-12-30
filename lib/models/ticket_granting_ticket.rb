6e4e01fa0e9ba8df5048d2aea586e6e0871775ff
Akash Manohar J <akash@akash.im>
2011-12-31 04:30:25 +0530

    Adds attr_accessible for required fields in ticket models
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/models/ticket_granting_ticket.rb
+++ lib/models/ticket_granting_ticket.rb
@@ -6,7 +6,7 @@ module CasFuji
       set_table_name "casfuji_tgt"
       has_many :service_tickets
 
-      attr_accessor :authenticator
+      attr_accessible :name, :permanent_id, :authenticator, :client_hostname
       
       validates_format_of      :name, :with => /\ATGT-[\w|\-]+\Z/
       # This should be used, but it's causing problem with the db locally
