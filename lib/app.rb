62a50ed2c89decac55dc629e05e7cd9ce440b529
Sean Grove <s@bushi.do>
2012-03-15 14:53:38 -0700

    Updates to notification implementation

--- lib/app.rb
+++ lib/app.rb
@@ -60,6 +60,7 @@ class CasFuji::App < Sinatra::Base
       set_tgt!(ticket_granting_ticket.name)
       
       if @service && !authorize_user!
+        user = User.find_by_email(@username)
         notify_activity("User unauthorized to access app", "#{user.email} signed in, but unauthorized to access #{@service}")
 
         halt(401, erb('unauthorized.html'.to_sym))
@@ -75,6 +76,7 @@ class CasFuji::App < Sinatra::Base
     end
 
     if @service and @errors.empty?
+      user = User.find_by_email(@username)
       notify_activity("User signed in (invite token)", "#{user.email} signed in for the #{user.sign_in_count.ordinalize} time, accessing #{@service}")
 
       @destination = url_with_ticket(@service, authenticator, permanent_id, @client_hostname, ticket_granting_ticket.id)
@@ -121,6 +123,7 @@ class CasFuji::App < Sinatra::Base
     end
 
     if @service and @errors.empty?
+      user = User.find_by_email(@username)
       notify_activity("User signed in (login)", "#{user.email} signed in for the #{user.sign_in_count.ordinalize} time to access #{@service}")
 
       halt(200, erb('redirect_warn.html'.to_sym)) if params[:warn]
@@ -274,15 +277,16 @@ class CasFuji::App < Sinatra::Base
 
   def notify_activity(title, body)
       begin
-        user = User.find_by_email(username)
         # hardcoded for production settings right now
-        notification         = Notification.new
-        notification.user_id = 1
-        notification.app_id  = 26250
-        notification.title   = title
-        notification.body    = body
-        notification.categor = "User Activity"
-        notification.save
+        notification          = Notification.new
+        notification.user_id  = 1
+        notification.app_id   = 26250
+        notification.title    = title
+        notification.body     = body
+        notification.category = "User Activity"
+        notification.save(:validate => false)
+
+        puts notification.inspect
       rescue => e
         puts "Couldn't save notification: #{e.inspect}"
       end
